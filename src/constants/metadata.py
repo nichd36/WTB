078c6d0394fb5334f8917d4b284f690a5235c021
Nicholas Dylan <ndylan76@gmail.com>
2026-06-28 22:50:24 +0800

    Init FastAPI app, / health check route

---
+++ src/constants/metadata.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+
+@dataclass(frozen=True)
+class FastAPIMetadata:
+    title: str = "Where's the Bus? (WTB) API"
+    summary: str = "Backend service for the WTB project"
+    description: str = "Aims to help provide bus passengers with a better commuting experience."
+    version: str = "0.0.0"
+    contact: dict = field(default_factory=lambda: {"name": "Nicholas D", "url": "https://github.com/nichd36"})
---
+++ src/server.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from constants.metadata import FastAPIMetadata
+
+# Create instance of FastAPIMetadata
+app_metadata = FastAPIMetadata()
+
+app = FastAPI(**app_metadata.__dict__)
+
+
+@app.get("/", tags=["Health"])
+def health():
+    return {
+        "message": "Let's commute!",
+        "name": "Where's the Bus? (WTB) API",
+        "version": "0.0.0",
+    }
